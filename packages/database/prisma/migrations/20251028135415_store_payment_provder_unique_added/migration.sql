3cdcea6a5df9802233685190ea49f7f2346eb39a
Kaan Akın <kaan.akin@systemsoft.local>
2025-11-06 11:03:28 +0300

    migrations-deleted

--- packages/database/prisma/migrations/20251028135415_store_payment_provder_unique_added/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[provder]` on the table `StorePaymentProvider` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "StorePaymentProvider_provder_key" ON "StorePaymentProvider"("provder");
--- packages/database/prisma/migrations/20251028190123_render_visible_type_added/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "VariantGroupRenderType" AS ENUM ('DROPDOWN', 'BADGE');
-
--- AlterTable
-ALTER TABLE "ProductVariantGroup" ADD COLUMN     "renderVisibleType" "VariantGroupRenderType" NOT NULL DEFAULT 'DROPDOWN';
