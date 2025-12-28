d038851ad0b7ea9a3bd521334d133479230f99a5
Kaan Akın <kaanakin@Kaans-MacBook-Air.local>
2025-12-28 14:02:52 +0300

    nixpacks-fixed

--- apps/backend/src/admin/carts/carts.module.ts
+++ apps/backend/src/admin/carts/carts.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CartsService } from './carts.service';
 import { CartsController } from './carts.controller';
+import { LocaleService } from 'src/common/services/locale.service';
 
 @Module({
   controllers: [CartsController],
-  providers: [CartsService],
+  providers: [CartsService, LocaleService],
 })
 export class CartsModule {}
